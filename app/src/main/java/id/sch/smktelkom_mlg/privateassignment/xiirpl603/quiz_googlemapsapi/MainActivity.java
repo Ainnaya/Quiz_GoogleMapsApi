67607ecfb859b7b01d32115894abbcc6706aa093
Ainnaya <Ainnaya56@gmail.com>
2017-11-26 22:51:36 +0700

    MapMove

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xiirpl603/quiz_googlemapsapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xiirpl603/quiz_googlemapsapi/MainActivity.java
@@ -16,6 +16,30 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    static final CameraPosition TBN = CameraPosition.builder()
+            .target(new LatLng(-6.895485, 112.029752))
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+    static final CameraPosition TOKYO = CameraPosition.builder()
+            .target(new LatLng(35.689487, 139.691706))
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+    static final CameraPosition NGAWI = CameraPosition.builder()
+            .target(new LatLng(-7.402921, 111.444668))
+            .zoom(20)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition OSAKA = CameraPosition.builder()
+            .target(new LatLng(34.693738, 135.502165))
+            .zoom(45)
+            .bearing(0)
+            .tilt(45)
+            .build();
     GoogleMap m_map;
     boolean mapReady = false;
 
@@ -29,7 +53,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    /* m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL); */
+                    flyTo(TOKYO);
             }
         });
 
@@ -38,7 +63,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                /*m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);*/
+                    flyTo(NGAWI);
             }
         });
 
@@ -47,7 +73,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    /*m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);*/
+                    flyTo(NGAWI);
             }
         });
 
@@ -61,9 +88,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         toast.show();
         mapReady = true;
         m_map = map;
-        LatLng newyork = new LatLng(40.712775, -74.005973);
+        /*LatLng newyork = new LatLng(40.712775, -74.005973);
         CameraPosition target = CameraPosition.builder().target(newyork).zoom(14).build();
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));*/
+        m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        flyTo(TBN);
+
     }
 
     @Override
@@ -81,5 +111,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void flyTo(CameraPosition target) {
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
     }
 }
